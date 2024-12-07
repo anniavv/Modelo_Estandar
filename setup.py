4cb75b65147877d9adb1a703672e9707ec7dae23
anniavv <annia_verdin@ciencias.unam.mx>
2024-12-06 20:55:47 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description="Un paquete sobre partículas del modelo estándar con interfaz gráfica usando PyQt5.",
     author="Annia Verdín",
     author_email="annia_verdin@ciencias.unam.mx",
-    url="https://github.com/tuusuario/First_Package",
+    url="https://github.com/anniavv/Modelo_Estandar.git",
     packages=find_packages(),
     install_requires=["PyQt5"],
     classifiers=[
